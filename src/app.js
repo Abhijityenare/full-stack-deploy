05d31cedfcfd8a005a4c480f447dba79ebb8975b
Abhijityenare <abhijityenare955@gmail.com>
2026-02-14 14:23:02 +0530

    add git ignore

---
+++ src/app.js
@@ -0,0 +1,52 @@
+const express = require('express')
+const noteModel = require('./models/note.model')
+const path = require('path')
+const app = express();
+const cors = require('cors')
+app.use(express.json());
+app.use(cors());
+app.use(express.static('./public'))
+app.post('/api/notes', async (req,res)=>{
+
+    const { title , description} = req.body
+
+     const note = await noteModel.create({
+        title,description
+    })
+
+    res.status(201).json({
+        message : "note creat successfully",
+        note
+    })
+})
+app.get('/api/notes',async(req,res)=>{
+     const notes = await noteModel.find()
+
+     res.status(200).json({
+        message:"notes fetch successfully",
+        notes
+     })
+})
+app.delete('/api/notes/:id',async(req,res)=>{
+    const id = req.params.id
+
+    console.log(id);
+    
+    await noteModel.findByIdAndDelete(id)
+
+    res.status(200).json({
+        message:"note deleted successfully",
+    })
+})
+app.patch('/api/notes/:id',async (req,res)=>{
+    const id = req.params.id;
+    const {description } = req.body
+    await noteModel.findByIdAndUpdate(id,{description})
+
+    res.status(200).json({
+        message:"note updated successfully",
+    })
+
+})
+
+module.exports = app;
